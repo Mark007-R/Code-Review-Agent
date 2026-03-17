7a2d153a8ea74516ab3acaf62d669835f3921ac7
Mark007-R <markrodrigues2004@gmail.com>
2026-03-18 00:29:14 +0530

    removing comments

--- ui/server.py
+++ ui/server.py
@@ -27,7 +27,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Serve static files (the index.html UI)
 static_dir = os.path.join(os.path.dirname(__file__), "static")
 os.makedirs(static_dir, exist_ok=True)
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
